e67bce9c82c29104cb6233553e2551538eab5148
Sai Sri Harsha Kanamanapalli <harshakanamanapalli@gmail.com>
2018-02-16 15:58:26 -0500

    Feat: Backend - Basic Routing Prototype

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config/config');
+const app = express();
+
+var account_type = 'user'; // TODO : get this from session
+
+const account_route = require('./routes/' + account_type);
+app.use('/' + account_type, account_route);
+
+/*
+app.get('/', function(req, res){
+    res.redirect('/' + account_type);
+});
+*/
+
+app.get('/*', function(req, res){
+    res.redirect('/' + account_type);
+});
+
+app.listen(config.dev.port, () =>{
+    console.log('Server running on locahost:' + config.dev.port);
+});
+
