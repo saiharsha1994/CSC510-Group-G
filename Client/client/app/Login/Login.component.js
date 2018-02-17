866278123b0ef08e877daf091c4e237292d61435
SANTHOSH <santhosh.dharmagadi@gmail.com>
2018-02-17 13:18:48 -0500

    chore(frontend: setup): initial setup and build
    Added basic build for frontend and also added login/signUp pages

---
+++ Client/client/app/Login/Login.component.js
@@ -0,0 +1,11 @@
+import template from './Login.html';
+import controller from './Login.controller.js'
+import './Login.component.scss';
+
+const LoginComponent = {
+  template,
+    controller,
+    controllerAs: 'vm'
+};
+
+export default LoginComponent;
