8694956993e52a5d506ddd22c169ee404e5cbe1f
SANTHOSH <santhosh.dharmagadi@gmail.com>
2018-02-18 16:56:46 -0500

    feat(enterprise-ui): added view for enterprise
    Added UI view for enterprise

--- Client/client/app/enterprise/enterprise.controller.js
+++ Client/client/app/enterprise/enterprise.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
+import enterpriseService from './enterprise.service';
+
 export default class enterpriseCtrl {
-    constructor($state, Upload) {
+    constructor($state, Upload, enterpriseService) {
         this.state = $state;
         this.uploadService = Upload;
+        this.enterpriseService = enterpriseService;
         this.selectedFile = {};
     }
 
@@ -16,15 +19,32 @@ export default class enterpriseCtrl {
         this.state.go('home');
     }
 
-    submit() {
-        console.log(this.selectedFile);
-        this.uploadService.upload({url: 'http://localhost:3000/uploadVideo', data:{file: this.selectedFile}})
+    addCoins() {
+        this.enterpriseService.addCoins(this.coinsToBeAdded)
             .then((response) => {
                 console.log(response);
             }).catch((response) => {
                 console.log(response);
             });
     }
+
+    updateCoinsPerHour() {
+        this.enterpriseService.updateCoinsPerHour(this.updatedCoinsPerHour)
+            .then((response) => {
+                console.log(response);
+            }).catch((response) => {
+            console.log(response);
+        });
+    }
+
+    uploadVideo() {
+        this.uploadService.upload({url: 'http://localhost:3000/uploadVideo', data:{file: this.selectedFile}})
+            .then((response) => {
+                console.log(response);
+            }).catch((response) => {
+            console.log(response);
+        });
+    }
 }
 
-enterpriseCtrl.$inject = ['$state', 'Upload'];
+enterpriseCtrl.$inject = ['$state', 'Upload', 'enterpriseService'];
