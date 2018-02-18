5e2e4f99db3c7c2dfc0906a7f5c1e73e25a4046e
skatta3 <skatta3@ncsu.edu>
2018-02-18 13:08:02 -0500

    feat : Backend - user and enterprise profile updation

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const path = require('path');
 const config = require('./config/config');
 const app = express();
 const cors = require('cors');
+var bodyParser = require('body-parser');
 
-var account_type = 'enterprise'; // TODO : get this from session
+var account_type = 'user';//'enterprise'; // TODO : get this from session
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const account_route = require('./routes/' + account_type);
 app.use('/' + account_type, account_route);
