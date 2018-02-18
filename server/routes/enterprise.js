8eeab1b73e2c39344be9f55b62121b2bd5781e0a
Sai Sri Harsha Kanamanapalli <harshakanamanapalli@gmail.com>
2018-02-18 12:29:12 -0500

    Feat : Backend - Video Upload
    This feature contains the code to upload the videos to database

--- server/routes/enterprise.js
+++ server/routes/enterprise.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 const User = require('../models/enterprise-model');
 
-var busboy = require('connect-busboy');
+//var busboy = require('connect-busboy');
 var fs = require('fs');
 
 let Grid = require('gridfs-stream');
@@ -23,7 +23,27 @@ mongoose.connect(config.dev.db, function(err){
 });
 
 connection.once("open", () => {
+    gfs = Grid(connection.db);
     
+    router.post('/uploadVideo', (req, res) => {
+        console.log('hip hip hurray');
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+            console.log("Uploading: " + filename + ' mimetype : ' + mimetype);
+            fstream = gfs.createWriteStream({
+                filename: filename,
+                mode: 'w'
+                //content_type: part.mimetype
+            });
+            //fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {
+                console.log('uploaded successfully');
+                res.redirect('back');
+            });
+        });
+    });
 });
 
 router.get('/', function(req, res){
