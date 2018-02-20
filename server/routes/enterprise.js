bf808ce36a200fb413db59cdb4c8ef100d2077e8
Sai Sri Harsha Kanamanapalli <harshakanamanapalli@gmail.com>
2018-02-19 23:24:46 -0500

    Added Frontend backend integration fixes

--- server/routes/enterprise.js
+++ server/routes/enterprise.js
@@ -99,10 +99,6 @@ router.post('/videoDetails', function (req, res) {
     });
 });
 
-router.get('/', function (req, res) {
-    res.send('Welcome to Enterprise home page!');
-});
-
 router.get('/details/:ename', function (req, res) {
     console.log(req.params.ename);
     Enterprise.findOne({ 'ename': req.params.ename }, function (err, record) {
@@ -135,17 +131,13 @@ router.get('/details/:ename', function (req, res) {
     });
 });
 
-router.get('/profile', function (req, res) {
-
-    res.send('Enterprise Profile page!');
-});
-
 router.post('/profile/update', function (req, res) {
     Enterprise.findOne({ "ename": req.body.username },
         (err, enterprise) => {
             if (err) {
                 res.status(400).send(err);
             }
+            // TODO : Handle this request
             if (enterprise && (req.body.password === enterprise.password)) {  // Search could come back empty, so we should protect against sending nothing back
                 enterprise.password = req.body.newpassword || enterprise.password;
                 enterprise.save((err, enterprise) => {
@@ -162,35 +154,11 @@ router.post('/profile/update', function (req, res) {
     );
 });
 
-router.post('/profile/update', function (req, res) {
-    console.log(req.body.enterpriseId);
-    var query = { "enterpriseId": req.body.enterprise }//, "emailId":req.body.emailId};
-    Enterprise.findOne(query, { "enterpriseId": true, "emailId": true },
-        (err, enterprise) => {
-            if (err) {
-                res.status(200).send(err)
-            }
-            if (enterprise && (req.body.password === enterprise.password)) {  // Search could come back empty, so we should protect against sending nothing back
-                enterprise.password = req.body.newpassword || enterpriseId.password;
-                enterprise.emailId = req.body.emailId || enterprise.emailId;
-                //enterprise.ename = req.body.ename || enterprise.ename; username cannot be updated as it is unique
-                enterprise.save((err, enterprise) => {
-                    if (err) {
-                        res.status(500).send(err)
-                    }
-                    res.status(200).send("enterprise details successfully updated" + enterprise);
-                });
-                //res.status(200).send(enterprise)
-            } else {  // In case no enterprise was found with the given query
-                res.status(200).send("No enterprise found")
-            }
-        }
-    );
-    //res.send('Enterprise Profile updated successfully'+req.body);
-});
 
 router.get('/stats', function (req, res) {
     res.send('Statistics of videos uploaded till now');
 });
+
+// TODO : Add route to delete video
 
 module.exports = router;
